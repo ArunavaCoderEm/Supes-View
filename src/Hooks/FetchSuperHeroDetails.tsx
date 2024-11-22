8ee43ae22e8320b9fb8bf1e5214a7be64cbbaddf
ArunavaCoderEm <acecankill@gmail.com>
2024-11-22 21:47:06 +0530

    a lot of things

--- src/Hooks/FetchSuperHeroDetails.tsx
+++ src/Hooks/FetchSuperHeroDetails.tsx
@@ -1,9 +1,38 @@
+import { newSuperheroApi } from "@/Api/Superhero";
 import { useQuery } from "@tanstack/react-query";
 
-export default function FetchSuperHeroDetailsQueryid (id: number | null) {
+export const HeroKeys = {
+    heroid: (id: number) => ["heroid", id] as const,
+    herosearch: (search: string) => ["heroseach", search] as const,
+    herosearchmore: (searchpar: string) => ["heroseachmore", searchpar] as const,
+}
 
-    useQuery({
-        queryKey: [id]
+export function FetchSuperHeroDetailsQueryid (id: number | null) {
+
+    return useQuery({
+        queryKey: HeroKeys?.heroid(id ?? 60),
+        queryFn: () => id ? newSuperheroApi?.getSuperHeroDetailbyId(id) : null,
+        enabled: !! id
     })
-  
+    
+}
+
+export function FetchSuperHeroDetailsQuerySearch (search: string | null) {
+
+    return useQuery({
+        queryKey: HeroKeys?.herosearch(search ?? "batman"),
+        queryFn: () => search ? newSuperheroApi?.getSuperHeroSearchResult(search) : null,
+        enabled: !! search
+    })
+
+}
+
+export function FetchSuperHeroDetailsQuerySearchMore (searchPars: (number | string[])[]) {
+
+    return useQuery({
+        queryKey: HeroKeys?.herosearch(String(searchPars[0]) ?? "70"),
+        queryFn: () => searchPars ? newSuperheroApi?.getSuperHeroIdMorehResult(searchPars) : null,
+        enabled: !! searchPars
+    })
+
 }
