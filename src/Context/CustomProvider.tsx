8a996e807ed977b71d4bda6cde8f76009032c128
ArunavaCoderEm <acecankill@gmail.com>
2024-11-22 15:53:06 +0530

    all files config done and dark toggle done

---
+++ src/Context/CustomProvider.tsx
@@ -0,0 +1,18 @@
+import { mainChildren } from "@/Types/types";
+import React, { StrictMode } from "react";
+import { ThemeProvider } from "./Theme-Provider";
+import { BrowserRouter } from "react-router-dom";
+
+export default function CustomProvider({
+  children,
+}: mainChildren): React.ReactNode {
+  return (
+    <BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <StrictMode>
+        {children}
+      </StrictMode>
+    </ThemeProvider>
+  </BrowserRouter>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./CSS/index.css";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import CustomProvider from "./Context/CustomProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <StrictMode>
+    <CustomProvider>
       <App />
-    </StrictMode>
-  </BrowserRouter>
+    </CustomProvider>
 );
