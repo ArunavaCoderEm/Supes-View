42e9a85a9aa9edc4320a4e24f608ac0050eb5d83
ArunavaCoderEm <acecankill@gmail.com>
2024-11-22 16:20:12 +0530

    implemented something

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Pages from "./Pages/Pages";
 
 function App(): React.ReactNode {
   return (
     <>
-
+      <Pages />
     </>
   );
 }
--- src/components/Web/Navbar.tsx
+++ src/components/Web/Navbar.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { useTheme } from '@/Context/Theme-Provider'
 
 export default function Navbar():React.ReactNode {
+
+    const { theme, setTheme } = useTheme();
+
   return (
+    <>
     <div>Navbar</div>
+    {theme === "dark" ? <Button onClick={() => {setTheme("light")}}>Light</Button> :
+        <Button onClick={() => {setTheme("dark")}}>Dark</Button>
+    }
+    
+    </>
   )
 }
