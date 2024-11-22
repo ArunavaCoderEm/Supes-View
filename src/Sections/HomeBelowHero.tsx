db7a401e6e639a5f6c1e5fecc0c856ae11817f5c
ArunavaCoderEm <acecankill@gmail.com>
2024-11-23 03:04:03 +0530

    totally done

--- src/Sections/HomeBelowHero.tsx
+++ src/Sections/HomeBelowHero.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -9,54 +10,119 @@ import {
 } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { FetchRandomSupesN } from "@/Hooks/FetchSuperHeroDetails";
-import React from "react";
+import { UseFav } from "@/Hooks/UseFav";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function HomeBelowHero(): React.ReactNode {
   const { data, isLoading, isError, error } = FetchRandomSupesN(12);
 
+  const { favs } = UseFav();
+
+  const [visibleCount, setVisibleCount] = useState<number>(4);
+
+  const loadMore = () => {
+    if (!isLoading) {
+      setVisibleCount(data?.length || 0);
+    }
+  };
+
   if (isError) {
     console.error("Error:", error);
   }
-  
+
   return (
     <>
       <div className="my-10 container">
         <h1 className="text-transparent text-5xl font-bold leading-tight bg-clip-text bg-gradient-to-br from-orange-500 to-orange-700 dark:from-orange-300 dark:to-orange-500">
-          Top Superheroes Today:
+          Your Favourite Superheroes :
         </h1>
 
-        {!isLoading && data && !isError && (
+        {!isLoading && favs && !isError && (
           <div className="grid mt-10 md:grid-cols-3 grid-cols-2 lg:grid-cols-4 gap-4">
-            {data.map((item, index) => (
-              <Link to={`/details/${item.id}`}>
-                <Card key={index}>
-                  <CardHeader>
-                    <img
-                      src={item?.image?.url}
-                      className="rounded-md shadow-md h-64 aspect-square"
-                      alt="HeroImg"
-                    />
-                    <div className="py-2">
-                      <CardTitle className="text-xl">{item.name}</CardTitle>
-                      <CardDescription>{item.name}</CardDescription>
-                    </div>
-                  </CardHeader>
-                  <CardContent className="flex flex-wrap gap-2">
-                    <Badge>{item?.appearance?.gender}</Badge>
-                    <Badge>{item?.appearance?.race}</Badge>
-                    <Badge>{item?.appearance?.height[0]}</Badge>
-                    <Badge>{item?.appearance?.weight[0]}</Badge>
-                  </CardContent>
-                  <CardFooter className="flex flex-col gap-2 justify-start">
-                    <p className="text-muted-foreground text-left">
-                      <span className="text-foreground">Pub : </span>
-                      {item?.biography?.publisher}
-                    </p>
-                  </CardFooter>
-                </Card>
-              </Link>
-            ))}
+            {favs.map((item, index) => {
+              return (
+                <>
+                  <Link to={`/details/${item.id}`} key={index}>
+                    <Card>
+                      <CardHeader>
+                        <img
+                          src={item?.image?.url}
+                          className="rounded-md shadow-md h-64 aspect-square"
+                          alt="HeroImg"
+                        />
+                        <div className="py-2">
+                          <CardTitle className="text-xl">{item.name}</CardTitle>
+                          <CardDescription>{item.name}</CardDescription>
+                        </div>
+                      </CardHeader>
+                      <CardContent className="flex flex-wrap gap-2">
+                        <Badge>{item?.appearance?.gender}</Badge>
+                        <Badge>{item?.appearance?.race}</Badge>
+                        <Badge>{item?.appearance?.height[0]}</Badge>
+                        <Badge>{item?.appearance?.weight[0]}</Badge>
+                      </CardContent>
+                      <CardFooter className="flex flex-col gap-2 justify-start">
+                        <p className="text-muted-foreground text-left">
+                          <span className="text-foreground">Pub : </span>
+                          {item?.biography?.publisher}
+                        </p>
+                      </CardFooter>
+                    </Card>
+                  </Link>
+                </>
+              );
+            })}
+          </div>
+        )}
+        <h1 className="text-transparent text-5xl font-bold leading-tight bg-clip-text bg-gradient-to-br from-orange-500 to-orange-700 dark:from-orange-300 dark:to-orange-500">
+          Top Superheroes Today:
+        </h1>
+        {!isLoading && data && !isError && (
+          <div>
+            <div className="grid mt-10 md:grid-cols-3 grid-cols-2 lg:grid-cols-4 gap-4">
+              {data &&
+                data?.length &&
+                data.slice(0, visibleCount).map((item, index) => (
+                  <Link to={`/details/${item.id}`} key={index}>
+                    <Card>
+                      <CardHeader>
+                        <img
+                          src={item?.image?.url}
+                          className="rounded-md shadow-md h-64 aspect-square"
+                          alt="HeroImg"
+                        />
+                        <div className="py-2">
+                          <CardTitle className="text-xl">{item.name}</CardTitle>
+                          <CardDescription>{item.name}</CardDescription>
+                        </div>
+                      </CardHeader>
+                      <CardContent className="flex flex-wrap gap-2">
+                        <Badge>{item?.appearance?.gender}</Badge>
+                        <Badge>{item?.appearance?.race}</Badge>
+                        <Badge>{item?.appearance?.height[0]}</Badge>
+                        <Badge>{item?.appearance?.weight[0]}</Badge>
+                      </CardContent>
+                      <CardFooter className="flex flex-col gap-2 justify-start">
+                        <p className="text-muted-foreground text-left">
+                          <span className="text-foreground">Pub : </span>
+                          {item?.biography?.publisher}
+                        </p>
+                      </CardFooter>
+                    </Card>
+                  </Link>
+                ))}
+            </div>
+            {visibleCount < data.length && (
+              <div className="mt-4 text-center">
+                <Button
+                  onClick={loadMore}
+                  className="px-4 py-2 bg-muted text-orange-500"
+                >
+                  Load More
+                </Button>
+              </div>
+            )}
           </div>
         )}
       </div>
--- src/Test/TestEnv.ts
+++ src/Test/TestEnv.ts
@@ -1,53 +1,50 @@
-// import { newSuperheroApi } from "@/Api/Superhero";
-
-// import { FetchRandomSupesN } from "@/Hooks/FetchSuperHeroDetails";
+import { newSuperheroApi } from "@/Api/Superhero";
 
 
-// export async function testGetSuperHeroDetail() {
-//     try {
-//         const superheroId = 30; 
-//         const superhero = await newSuperheroApi.getSuperHeroDetailbyId(superheroId);
-//         console.log(superhero); 
-//     } catch (error) {
-//         console.error("Error fetching superhero details: ", error);
-//     }
-// }
 
+export async function testGetSuperHeroDetail() {
+    try {
+        const superheroId = 30; 
+        const superhero = await newSuperheroApi.getSuperHeroDetailbyId(superheroId);
+        console.log(superhero); 
+    } catch (error) {
+        console.error("Error fetching superhero details: ", error);
+    }
+}
 
-// export async function testGetSearchSuperHeroDetail() {
-//     try {
-//         const search = "Ant Man"; 
-//         const superhero = await newSuperheroApi.getSuperHeroSearchResult(search);
-//         console.log(superhero); 
-//     } catch (error) {
-//         console.error("Error fetching superhero details: ", error);
-//     }
-// }
+// testGetSuperHeroDetail()
 
+export async function testGetSearchSuperHeroDetail() {
+    try {
+        const search = "Ant Man"; 
+        const superhero = await newSuperheroApi.getSuperHeroSearchResult(search);
+        console.log(superhero); 
+    } catch (error) {
+        console.error("Error fetching superhero details: ", error);
+    }
+}
 
-// export async function testgetSuperHeroIdMorehResult() {
-//     try {
-//         const search = [70, ["appearance"]]; 
-//         const superhero = await newSuperheroApi.getSuperHeroIdMorehResult(search);
-//         console.log(superhero); 
-//     } catch (error) {
-//         console.error("Error fetching superhero details: ", error);
-//     }
-// }
+// testGetSearchSuperHeroDetail()
+
+export async function testgetSuperHeroIdMorehResult() {
+    try {
+        const search = [70, ["appearance"]]; 
+        const superhero = await newSuperheroApi.getSuperHeroIdMorehResult(search);
+        console.log(superhero); 
+    } catch (error) {
+        console.error("Error fetching superhero details: ", error);
+    }
+}
 
 // testgetSuperHeroIdMorehResult();
 
-// export async function getrandomSuperhero() {
-//     try {
-//         const superhero = await newSuperheroApi.getNRandomSupes(10);
-//         console.log(superhero.length); 
-//     } catch (error) {
-//         console.error("Error fetching superhero details: ", error);
-//     }
-// }
+export async function getrandomSuperhero() {
+    try {
+        const superhero = await newSuperheroApi.getNRandomSupes(10);
+        console.log(superhero.length); 
+    } catch (error) {
+        console.error("Error fetching superhero details: ", error);
+    }
+}
 
 // getrandomSuperhero();
-
-// const res = FetchRandomSupesN(2);
-
-// console.log(res);
